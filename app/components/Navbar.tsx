855f6dcc92863c17fa902a8f903be850a022ecb0
CampbellKemp24 <fredthehog@gmail.com>
2026-02-03 01:14:45 -0500

    Added Login Screen
    Added Login Screen

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,9 +9,9 @@ export default async function Navbar() {
   });
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center sticky top-0">
-      <div className="text-xl font-bold">
-        <Link href="/">MySite</Link>
+    <nav className="bg-[#003862] text-[#ffffff] p-4 flex justify-between items-center sticky top-0">
+      <div className="text-lg font-light tracking-[0.18em] uppercase text-white">
+        <Link href="/">TIGER TRUCK TRANSIT</Link>
       </div>
       
       {session?.user ? (
@@ -22,20 +22,23 @@ export default async function Navbar() {
           <LogoutButton />
         </div>
       ) : (
-        <div className="space-x-4">
-          <Link
-            href="/login"
-            className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-100 transition"
-          >
-            Login
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-green-500 px-4 py-2 rounded hover:bg-green-600 transition"
-          >
-            Sign Up
-          </Link>
-        </div>
+     <div className="space-x-4">
+      <Link
+        href="/login"
+        className="px-6 py-2 rounded-full border border-white text-white
+                  hover:bg-white/10 transition-colors duration-200"
+      >
+        Login
+      </Link>
+
+      <Link
+        href="/signup"
+        className="px-6 py-2 rounded-full bg-white text-[#003862]
+                  hover:bg-white/90 transition-colors duration-200"
+      >
+        Sign Up
+      </Link>
+    </div>
       )}
     </nav>
   );
