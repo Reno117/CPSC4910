501786765da4fd69f3b0ffa463058bbf727982d0
Bennett <bkalexa@clemson.edu>
2026-02-22 15:30:31 -0500

    Added ability for sponsor to change point conversion

--- app/sponsor/catalog/page.tsx
+++ app/sponsor/catalog/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import CatalogActions from "@/app/components/catalog/catalog-actions";
 import CatalogSearch from "@/app/components/catalog/search-catalog";
 import ProductCard from "@/app/components/catalog/catalog-product-card";
+import UpdatePointsModal from "@/app/components/catalog/edit-pointconversion-button";
 
 export default async function CatalogPage({
   searchParams,
@@ -11,6 +12,15 @@ export default async function CatalogPage({
   searchParams: Promise<{ search?: string }>;
 })  {
   const { isAdmin, sponsorId } = await requireSponsorOrAdmin();
+  let pointConversion = null;
+
+if (sponsorId) {
+  const sponsor = await prisma.sponsor.findUnique({
+    where: { id: sponsorId },
+    select: { pointValue: true },
+  });
+  pointConversion = sponsor?.pointValue ?? null;
+}
   const { search } = await searchParams;
   const searchQuery = search || "";
   const products = await prisma.catalogProduct.findMany({
@@ -49,12 +59,15 @@ export default async function CatalogPage({
             Manage products that drivers can redeem with points
           </p>
         </div>
+        <div style={{display: 'flex', gap: '10px', alignItems: 'center'}}>
+        <UpdatePointsModal itemId={sponsorId} pointConversion={pointConversion} />
         <Link
           href="/sponsor/catalog/add"
           className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition font-medium"
         >
           + Add Products
         </Link>
+        </div>
       </div>
       <CatalogSearch initialSearch={searchQuery} />
       {/* Stats */}
